a209859539e87b4de0c69e57cc46008f92cd76ad
devkont <yusupovb@gmail.com>
2015-04-02 09:25:29 +0300

    fixed #6

--- sbr/sbr.global.php
+++ sbr/sbr.global.php
@@ -17,24 +17,46 @@ if ($sbrpays = cot_payments_getallpays('sbr', 'paid'))
 	{
 		if (cot_payments_updatestatus($pay['pay_id'], 'done'))
 		{
-			$sbr = $db->query("SELECT * FROM $db_sbr WHERE sbr_id=" . $pay['pay_code'])->fetch();
-			
-			// Запуск сделки на исполнение
-			$db->update($db_sbr, array('sbr_status' => 'process', 'sbr_begin' => $sys['now']), "sbr_id=" . $pay['pay_code']);
-			
-			// Активируем на исполнение первый этап сделки
-			$db->update($db_sbr_stages, array('stage_status' => 'process', 'stage_begin' => $sys['now']), "stage_sid=" . $pay['pay_code'] . " AND stage_num=1");
-			
-			// Отправка уведомлений
-			cot_sbr_sendpost($pay['pay_code'], $L['sbr_posts_performer_paid'], $sbr['sbr_performer'], 0, 'success', true);
-			cot_sbr_sendpost($pay['pay_code'], $L['sbr_posts_employer_paid'], $sbr['sbr_employer'], 0, 'success', true);
-			
-			/* === Hook === */
-			foreach (cot_getextplugins('sbr.pay.done') as $pl)
-			{
-				include $pl;
+			if($sbr = $db->query("SELECT * FROM $db_sbr WHERE sbr_id=" . $pay['pay_code'])->fetch()){
+
+				// Запуск сделки на исполнение
+				if($db->update($db_sbr, array('sbr_status' => 'process', 'sbr_begin' => $sys['now']), "sbr_id=" . $pay['pay_code'])){
+
+					// Выбираем исполнителем, если сделка привязана к проекту
+					if($sbr['sbr_pid'] > 0){
+						
+						// находим предыдущего выбранного исполнителя, если есть
+						$lastperformer = $db->query("SELECT u.* FROM $db_projects_offers AS o
+							LEFT JOIN $db_users AS u ON u.user_id=o.item_userid 
+							WHERE item_pid=" . (int)$sbr['sbr_pid'] . " AND item_choise='performer'")->fetch();
+
+						if($db->update($db_projects_offers, array("item_choise" => 'performer', "item_choise_date" => (int)$sys['now']), "item_pid=" . (int)$sbr['sbr_pid'] . " AND item_userid=" . (int)$sbr['sbr_performer'])){
+							if($db->update($db_projects, array("item_performer" => $sbr['sbr_performer']), "item_id=" . (int)$sbr['sbr_pid'])){
+								/* === Hook === */
+								foreach (cot_getextplugins('sbr.pay.setperformer') as $pl)
+								{
+									include $pl;
+								}
+								/* ===== */
+							}
+						}
+					}
+
+					// Активируем на исполнение первый этап сделки
+					$db->update($db_sbr_stages, array('stage_status' => 'process', 'stage_begin' => $sys['now']), "stage_sid=" . $pay['pay_code'] . " AND stage_num=1");
+
+					// Отправка уведомлений
+					cot_sbr_sendpost($pay['pay_code'], $L['sbr_posts_performer_paid'], $sbr['sbr_performer'], 0, 'success', true);
+					cot_sbr_sendpost($pay['pay_code'], $L['sbr_posts_employer_paid'], $sbr['sbr_employer'], 0, 'success', true);
+
+					/* === Hook === */
+					foreach (cot_getextplugins('sbr.pay.done') as $pl)
+					{
+						include $pl;
+					}
+					/* ===== */
+				}
 			}
-			/* ===== */
 		}
 	}
 }
